b81475fbdd6fbb3a9a3ca3ab587eefa51cb2d7f4
sidshekhar24 <ss2744@cornell.edu>
2018-01-02 03:08:05 +0000

    testing data

--- scripts/loadBlockchain.js
+++ scripts/loadBlockchain.js
@@ -5,6 +5,8 @@ const EthTransaction = require('../models/Ethereum/EthTransaction');
 const EthTransactionReceipt = require('../models/Ethereum/EthTransactionReceipt');
 const EthLog = require('../models/Ethereum/EthLog');
 var Promise = require('bluebird');
+var bookshelf = require('../config/bookshelf');
+const knex = require('knex');
 
 dotenv.load();
 
@@ -13,69 +15,76 @@ const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io
 starting_block = process.argv[2];
 ending_block = process.argv[3];
 
-for (var i = 300; i < 1000; i++) {
-  web3.eth
-    .getBlock(i)
-    .then(function(block) {
-      console.log(i);
-      blockData = create_block_obj(block);
-      EthBlock.forge(blockData)
-        .save(null, { method: 'insert' })
-        .then(function(savedBlock) {
-          console.log('saving block');
-          if (block.transactions.length != 0) {
-            console.log(block.transactions.length, '# of txxx');
-            for (var t = 0; t < block.transactions.length; t++) {
-              console.log(t, block.transactions[t], 'looping');
-              web3.eth
-                .getTransaction(block.transactions[t])
-                .then(function(transaction) {
-                  txnData = create_transaction_obj(transaction, savedBlock.id);
-                  EthTransaction.forge(txnData)
-                    .save(null, { method: 'insert' })
-                    .then(function(savedTransaction) {
-                      web3.eth
-                        .getTransactionReceipt(transaction.hash)
-                        .then(function(transactionReceipt) {
-                          txnReceiptData = create_transaction_receipt_obj(transactionReceipt, savedTransaction.id);
-                          EthTransactionReceipt.forge(txnReceiptData)
-                            .save(null, { method: 'insert' })
-                            .then(function(savedTrasnactionReceipt) {
-                              //LOGS
-                              if (transactionReceipt.logs != []) {
-                                for (var l = 0; l < transactionReceipt.logs.length; l++) {
-                                  logsObj = create_logs_obj(transactionReceipt.logs[l], savedTrasnactionReceipt.id);
-                                  EthLog.forge(logsObj).save(null, { method: 'insert' }).catch(function(err) {
-                                    console.log(err, 'error saving log', transactionReceipt.logs[l].address);
-                                  });
+counter = 0;
+
+loadEthBlockchain(starting_block, ending_block);
+
+function loadEthBlockchain(start, end) {
+  for (var i = start; i < end; i++) {
+    web3.eth
+      .getBlock(i)
+      .then(function(block) {
+        blockData = create_block_obj(block);
+        EthBlock.forge(blockData)
+          .save(null, { method: 'insert' })
+          .then(function(savedBlock) {
+            console.log('saving block');
+            if (block.transactions.length) {
+              console.log(block.transactions.length, '# of txxx');
+              for (var t = 0; t < block.transactions.length; t++) {
+                console.log(t, block.transactions[t], 'looping');
+                web3.eth
+                  .getTransaction(block.transactions[t])
+                  .then(function(transaction) {
+                    txnData = create_transaction_obj(transaction, savedBlock.id);
+                    EthTransaction.forge(txnData)
+                      .save(null, { method: 'insert' })
+                      .then(function(savedTransaction) {
+                        web3.eth
+                          .getTransactionReceipt(transaction.hash)
+                          .then(function(transactionReceipt) {
+                            txnReceiptData = create_transaction_receipt_obj(transactionReceipt, savedTransaction.id);
+                            EthTransactionReceipt.forge(txnReceiptData)
+                              .save(null, { method: 'insert' })
+                              .then(function(savedTransactionReceipt) {
+                                //LOGS
+                                if (transactionReceipt.logs != []) {
+                                  for (var l = 0; l < transactionReceipt.logs.length; l++) {
+                                    logsObj = create_logs_obj(transactionReceipt.logs[l], savedTransactionReceipt.id);
+                                    EthLog.forge(logsObj).save(null, { method: 'insert' }).catch(function(err) {
+                                      console.log(err, 'error saving log', transactionReceipt.logs[l].address);
+                                    });
+                                  }
                                 }
-                              }
-                            })
-                            .catch(function(err) {
-                              console.log(err, 'error saving txReceitpt', transaction.hash);
-                            });
-                        })
-                        .catch(function(err) {
-                          console.log(err, 'error getting txReceitpt', transaction.hash);
-                        });
-                    })
-                    .catch(function(err) {
-                      console.log(err, 'error saving transaction', transaction.hash);
-                    });
-                })
-                .catch(function(err) {
-                  console.log(err, 'error getting transaction', block.transactions[t]);
-                });
+                              })
+                              .catch(function(err) {
+                                console.log(err, 'error saving txReceitpt', transaction.hash);
+                              });
+                          })
+                          .catch(function(err) {
+                            console.log(err, 'error getting txReceitpt', transaction.hash);
+                          });
+                      })
+                      .catch(function(err) {
+                        console.log(err, 'error saving transaction', transaction.hash);
+                      });
+                  })
+                  .catch(function(err) {
+                    console.log(err, 'error getting transaction', block.transactions[t]);
+                  });
+              }
             }
-          }
-        })
-        .catch(function(err) {
-          console.log(err, 'error saving block', i);
-        });
-    })
-    .catch(function(err) {
-      console.log(err, 'error getting block', i);
-    });
+          })
+          .catch(function(err) {
+            console.log(err, 'error saving block', i);
+          });
+        counter++;
+        console.log('counter: ', counter);
+      })
+      .catch(function(err) {
+        console.log(err, 'error getting block', i);
+      });
+  }
 }
 
 function create_block_obj(blockObj) {
